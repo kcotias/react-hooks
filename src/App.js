5d70042c170047bdd274eec703e2009c7a8173c7
kcotias <kaianexperia@gmail.com>
2019-05-16 23:27:47 -0300

    example of how use set on hooks / and how to populate an array with new items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 function App() {
   const [repositories, setRepositories] = useState([]);
 
+  function handleRepository() {
+    setRepositories([...repositories, { id: 'novo', name: 'novo' }])
+  }
+
   return (
     <div className="App">
       <ul>
@@ -10,6 +14,9 @@ function App() {
           <li key={repo.id}>{repo.name}</li>
         ))}
       </ul>
+      <button onClick={handleRepository}>
+        Add repo
+      </button>
     </div>
   );
 }
