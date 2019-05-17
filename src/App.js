8d1493af14001dadf30556860cc3f97ba7f4c67f
kcotias <kaianexperia@gmail.com>
2019-05-16 23:34:26 -0300

    import useeffect and add comments of its functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  // use effect is made to replace setState and initial state setting
   // first parameter is the title os the state variable
   // second parameter is the title of the function we will use to change its value
   //  then we call useState([]) and inside of it we put the default value it will have
   const [repositories, setRepositories] = useState([]);
+
+  // use effect is made to replace components life cycle methods
+  // first parameter is the function it will run (the body of componentdidmount and so on...)
+  // second parameter should be an array of values that the function will watch, 
+  //and everytime one of those values changes, that function of the first parameter will run. 
+  // tips: if you just want the component didmount funcionality, just live the second parameter as an empty array.
+  useEffect(() => {
+
+  }, [])
 
   return (
     <div className="App">
